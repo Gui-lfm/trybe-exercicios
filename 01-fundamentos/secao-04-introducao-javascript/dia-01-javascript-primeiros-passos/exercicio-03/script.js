583575ac934cc77fc48d77c0d1b64abd4d7b3247
Guilherme Lucena <guilherme.lucena17@gmail.com>
2022-09-21 16:38:17 -0300

    exercicio-03 finalizado

---
+++ 01-fundamentos/secao-04-introducao-javascript/dia-01-javascript-primeiros-passos/exercicio-03/script.js
@@ -0,0 +1,9 @@
+const notaAluno = 57;
+
+if (notaAluno >= 80) {
+    console.log("Parabéns, você foi aprovada(o)!");
+} else if (notaAluno >= 60 && notaAluno < 80) {
+    console.log("Você está na nossa lista de espera.");
+} else {
+    console.log("Você foi reprovada(o).")
+}
