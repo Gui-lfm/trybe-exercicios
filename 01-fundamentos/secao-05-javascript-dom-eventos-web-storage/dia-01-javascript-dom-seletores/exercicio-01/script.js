9efac736049f540efa1a4f544762727c54c43f3f
Guilherme Lucena <guilherme.lucena17@gmail.com>
2022-09-28 17:24:34 -0300

    passos 5 e 6 finalizados

--- 01-fundamentos/secao-05-javascript-dom-eventos-web-storage/dia-01-javascript-dom-seletores/exercicio-01/script.js
+++ 01-fundamentos/secao-05-javascript-dom-eventos-web-storage/dia-01-javascript-dom-seletores/exercicio-01/script.js
@@ -7,20 +7,37 @@ function MudaParagrafo(string) {
 MudaParagrafo('em dois anos me vejo morando na praia')
 
 // parte 2 e 3
-
-function mudaBg(element, cor){
+function mudaBg(element, cor) {
     let quadrado = document.getElementsByClassName(element);
-   quadrado[0].style.backgroundColor = cor;
+    quadrado[0].style.backgroundColor = cor;
 }
 
-mudaBg('main-content','rgb(76,164,109)');
+mudaBg('main-content', 'rgb(76,164,109)');
 mudaBg('center-content', 'white')
 
 // parte 4
-
-function mudaTitulo(string){
+function mudaTitulo(string) {
     let title = document.getElementsByClassName('title')[0]
     title.innerText = string
 }
 
-mudaTitulo('Exercício - JavaScript')
+mudaTitulo('Exercício 5.1 - JavaScript')
+
+// parte 5
+function textoMaiusculo(element) {
+    let maiusculo = document.getElementsByTagName(element)
+    maiusculo[0].style.textTransform = 'uppercase'
+}
+
+textoMaiusculo('p')
+
+// parte 6
+function exibeConteudo(element) {
+    let tag = document.getElementsByTagName(element);
+
+    for (let i = 0; i < tag.length; i += 1) {
+        console.log(tag[i].innerHTML)
+    }
+}
+
+exibeConteudo('p')
