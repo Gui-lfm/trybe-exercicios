dad78331233328d76bf7a007025ece20df4df6c4
Guilherme Lucena <guilherme.lucena17@gmail.com>
2022-10-17 20:55:26 -0300

    parte final do exercicio 02 concluida

--- 01-fundamentos/secao-07-introducao-a-javascript-ES6-e-testes-unitarios/dia-01-let-const-arrow-functions-template-literals/exercicio-02/script.js
+++ 01-fundamentos/secao-07-introducao-a-javascript-ES6-e-testes-unitarios/dia-01-let-const-arrow-functions-template-literals/exercicio-02/script.js
@@ -16,35 +16,55 @@ const fat = (number) => (number === 1 ? 1 : number * fat(number - 1));
 console.log(fat(5));
 
 // parte 2
-const exemplo = "Antônio foi ao banheiro e não sabemos o que aconteceu"; 
+const exemplo = "Antônio foi ao banheiro e não sabemos o que aconteceu";
 
 const longestWord = (string) => {
   const arrayPalavras = string.split(" ");
   let nLetras = 0;
   let maiorPalavra;
   for (const palavra of arrayPalavras) {
-    if(palavra.length > nLetras){
+    if (palavra.length > nLetras) {
       nLetras = palavra.length;
       maiorPalavra = palavra;
     }
   }
-  return maiorPalavra
+  return maiorPalavra;
 };
 
 console.log(longestWord(exemplo));
 
 // parte 3
-const btnCounter = document.getElementById('btn-counter');
-const counterText = document.getElementById('num-cliques');
+const btnCounter = document.getElementById("btn-counter");
+const counterText = document.getElementById("num-cliques");
 
 let counter = 0;
 
-btnCounter.addEventListener('click', ()=>{
-  counter +=1;
+btnCounter.addEventListener("click", () => {
+  counter += 1;
   counterText.innerText = counter;
-})
+});
 
 // parte 4
-const substituaX = ()=>{
+const substituaX = (nome) => {
+  const frase = "Tryber x aqui!";
+  const palavras = frase.split(" ");
+  for (let i = 0; i < palavras.length; i += 1) {
+    if (palavras[i] === 'x') {
+      palavras[i] = nome;
+    }
+  }
+  return palavras.join(" ");
+};
+
+const minhasSkills = (func)=>{
+  const tryber = func;
+  const skills = ['javascript', 'html', 'css'];
+
+  return `  ${tryber}
+
+  Minhas três principais habilidades são:
   
+  ${skills}`;
 }
+
+console.log(minhasSkills(substituaX('guilherme')));
