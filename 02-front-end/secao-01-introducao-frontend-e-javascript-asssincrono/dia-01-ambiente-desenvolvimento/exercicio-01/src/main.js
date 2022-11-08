1bf960ee8af9b135c2303c688f866ca72abb41ca
Guilherme Lucena <guilherme.lucena17@gmail.com>
2022-11-08 20:26:01 -0300

    aplicacao de copy-clipboard

--- 02-front-end/secao-01-introducao-frontend-e-javascript-asssincrono/dia-01-ambiente-desenvolvimento/exercicio-01/src/main.js
+++ 02-front-end/secao-01-introducao-frontend-e-javascript-asssincrono/dia-01-ambiente-desenvolvimento/exercicio-01/src/main.js
@@ -1,10 +1,17 @@
 import { nanoid } from 'nanoid';
+import copy from 'clipboard-copy';
 import './style.css';
 
-const btnSafePassword = document.querySelector('button');
+const btnSafePassword = document.getElementById('generate-password');
 const passwordField = document.querySelector('h2');
+const copyPassword = document.getElementById('copy');
 
 btnSafePassword.addEventListener('click', () => {
   const randomPassWord = nanoid(10);
   passwordField.innerText = randomPassWord;
+});
+
+copyPassword.addEventListener('click', async ()=>{
+  await copy(passwordField.innerText);
+  alert('senha copiada');
 });
