e24d7f911fb5ac97aeec615d23f1b95559156780
Guilherme Lucena <guilherme.lucena17@gmail.com>
2022-11-23 12:38:15 -0300

    exercicio 01 finalizado

---
+++ 02-front-end/secao-02-introducao-react/dia-01-hello-world-no-react/teste-um/src/about.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class About extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Guilherme Lucena</h1>
+        <p>Estudante da trybe - turma 27</p>
+        <h2>Habilidades</h2>
+        <ul>
+          <li>Javascript</li>
+          <li>HTML</li>
+          <li>TDD</li>
+          <li>React</li>
+        </ul>
+      </>
+    );
+  }
+}
+
+export default About;
