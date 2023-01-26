d63889b5114901e0ddbdc300b36d26a54722bdb9
Guilherme Lucena <guilherme.lucena17@gmail.com>
2023-01-26 14:23:19 -0300

    exercicio 01 concluido

--- 02-front-end/secao-07-gerenciamento-de-estado-com-redux/dia-01-introducao-ao-redux-estado-global-da-aplicacao/exercicio-01/index.js
+++ 02-front-end/secao-07-gerenciamento-de-estado-com-redux/dia-01-introducao-ao-redux-estado-global-da-aplicacao/exercicio-01/index.js
@@ -3,14 +3,42 @@ import { composeWithDevTools } from '@redux-devtools/extension';
 
 const NEXT_COLOR = 'NEXT_COLOR';
 const PREVIOUS_COLOR = 'PREVIOUS_COLOR';
+const RANDOM_COLOR = 'RANDOM_COLOR';
 
 const INITIAL_STATE = {
   colors: ['white', 'black', 'red', 'green', 'blue', 'yellow'],
   index: 0,
 };
 
+function criarCor() {
+  const oneChar = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+  ];
+  let cor = '#';
+  const aleatorio = () => Math.floor(Math.random() * oneChar.length);
+  for (let i = 0; i < 6; i += 1) {
+    cor += oneChar[aleatorio()];
+  }
+  return cor;
+}
+
 const actionNextColor = { type: NEXT_COLOR };
 const actionPreviousColor = { type: PREVIOUS_COLOR };
+const actionRandomColor = { type: RANDOM_COLOR };
 
 const reducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
@@ -25,6 +53,12 @@ const reducer = (state = INITIAL_STATE, action) => {
         ...state,
         index: state.index === 0 ? state.colors.length - 1 : state.index - 1,
       };
+
+    case RANDOM_COLOR:
+      return {
+        colors: [...state.colors, criarCor()],
+        index: state.colors.length,
+      };
     default:
       return state;
   }
@@ -34,6 +68,7 @@ const store = createStore(reducer, composeWithDevTools());
 
 const nextBtn = document.querySelector('#next');
 const previousBtn = document.querySelector('#previous');
+const randomBtn = document.querySelector('#random');
 
 nextBtn.addEventListener('click', () => {
   store.dispatch(actionNextColor);
@@ -41,6 +76,10 @@ nextBtn.addEventListener('click', () => {
 
 previousBtn.addEventListener('click', () => {
   store.dispatch(actionPreviousColor);
+});
+
+randomBtn.addEventListener('click', () => {
+  store.dispatch(actionRandomColor);
 });
 
 store.subscribe(() => {
